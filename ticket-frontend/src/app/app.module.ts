6df8c736669fb9809880d5dd382d23c00c0eb672
miraj <miraj00@gmail.com>
2023-03-28 11:48:21 -0400

    new-ticket and bookmark-list components created

--- ticket-frontend/src/app/app.module.ts
+++ ticket-frontend/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { BookmarkListComponent } from './bookmark-list/bookmark-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTicketComponent,
+    BookmarkListComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
